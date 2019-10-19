519c4b53a45ee66abbf782adcf0e4d3a02ae0822
MCCshreyas <shreyasjejurkar123@live.com>
2019-10-20 00:29:55 +0530

    Added Loader interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { LoaderComponent } from '../loader/loader.component';
+import { LoaderService } from '../loader.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:
+    [
+      BrowserModule,
+      FormsModule,
+      MatProgressSpinnerModule
+    ],
+  providers: [LoaderService],
+  declarations: [AppComponent, LoaderComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
